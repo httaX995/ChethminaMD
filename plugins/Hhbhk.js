df1af6b204467f8606d96c26ca6a4484deb29a03
httaX995 <httx995@gmail.com>
2025-02-14 23:29:04 +0530

    Update Hhbhk.js

--- plugins/Hhbhk.js
+++ plugins/Hhbhk.js
@@ -1,5 +1,7 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
 cmd({
-    pattern: `smovie`,
+    pattern: "smovie",
     react: "📥",
     description: "movie downloader",
     use: ".movie kgf",
